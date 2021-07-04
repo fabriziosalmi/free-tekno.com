11938b585779e557ee8d5c8599b9283160173419
fab <fabrizio.salmi@gmail.com>
2021-07-04 13:17:02 +0200

    Update start.sh

--- streaming/youtube/start.sh
+++ streaming/youtube/start.sh
@@ -18,6 +18,6 @@ ffmpeg \
     -vf drawtext="fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf: fontsize=55: \
      box=0: boxcolor=black@0.5: boxborderw=20: \
      textfile=$NP_SOURCE: reload=1: fontcolor=white@0.8: x=315: y=960" \
-    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
-    -acodec libmp3lame -ar 44100 -threads 6 -qscale:v 3 -b:a 320000 -bufsize 512k \
+    -vcodec libx264 -pix_fmt yuv420p -threads 2 -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+    -acodec libmp3lame -ar 44100 -threads 2 -qscale:v 3 -b:a 320000 -bufsize 512k \
     -f flv "$YOUTUBE_URL/$KEY"
